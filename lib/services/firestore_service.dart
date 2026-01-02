9dfd073d707d28be7f977d6e206d5d424c9cff9d
Fajri Khaerullah <fajrikhaerullah@MacBook-Pro-Fajri.local>
2026-01-02 20:12:25 +0700

    feat(service): Buat kerangka kelas FirestoreService

---
+++ lib/services/firestore_service.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tugas_mobile/models/atlet.dart';
+
+// Kelas ini bertanggung jawab untuk semua operasi terkait Firestore.
+class FirestoreService {
+  
+}
