a5c2afde81108f03597f6874163176b1d8b90199
Fajri Khaerullah <fajrikhaerullah@MacBook-Pro-Fajri.local>
2026-01-02 20:11:33 +0700

    feat(model): Tambahkan properti dan constructor ke model Atlet

--- lib/models/atlet.dart
+++ lib/models/atlet.dart
@@ -2,5 +2,21 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 // Kelas model untuk merepresentasikan data seorang atlet.
 class Atlet {
+  final String? id;
+  final String nama;
+  final String cabangAtlet;
+  final int umur;
+  final String jenisKelamin;
+  final double beratBadan;
+  final double tinggiBadan;
 
+  Atlet({
+    this.id,
+    required this.nama,
+    required this.cabangAtlet,
+    required this.umur,
+    required this.jenisKelamin,
+    required this.beratBadan,
+    required this.tinggiBadan,
+  });
 }
