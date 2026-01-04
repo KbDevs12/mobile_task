433ca85d80391202f85668bc18bbf1eab940e3ff
annddvaa <anandadava93@gmail.com>
2026-01-05 01:46:13 +0700

    menyelesaikan info card

--- lib/widgets/info_card.dart
+++ lib/widgets/info_card.dart
@@ -16,6 +16,30 @@ class InfoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Expanded(
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Icon(icon, size: 36, color: color),
+              const SizedBox(height: 8),
+              Text(
+                value,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(title),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
