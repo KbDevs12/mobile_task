d6584cadd4315c070b3c1bc671ae0ed536ef0a1c
annddvaa <anandadava93@gmail.com>
2026-01-05 02:40:11 +0700

    menyelesaikan layout dashboard utama

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
+import '../views/atlet_list_screen.dart';
+import 'dashboard_content.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = const [DashboardContent(), AtletListScreen()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            appBar: AppBar(
-              title: const Text("Dashboard"),
-              actions: const [],
-            ),
-            body: SingleChildScrollView(
-              child: Container(
-                padding: const EdgeInsets.all(20.0),
-                child: const Column(
-                  children: [],
-                ),
-              ),
-            ),
-          );
+      appBar: AppBar(title: const Text("Dashboard")),
+      body: IndexedStack(index: _selectedIndex, children: _pages),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.teal,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Atlet'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profil'),
+        ],
+      ),
+    );
   }
 }
