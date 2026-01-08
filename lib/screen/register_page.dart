6d6f3d9ba293de90f68dc3ebaa9a0a92f12f0d9d
KbDevs12 <daniaku203@gmail.com>
2026-01-08 23:20:22 +0700

    membuat halaman register

---
+++ lib/screen/register_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+  bool obscurePassword = true;
+  bool isLoading = false;
+}
