4aaf7dbcb47ac0a3ae0014ea84f8b33cc4bee340
Fajri Khaerullah <fajrikhaerullah@MacBook-Pro-Fajri.local>
2026-01-08 19:10:30 +0700

    fajri: Move Atlet CRUD operations to AtletService and clear FirestoreService

---
+++ lib/services/atlet_service.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tugas_mobile/models/atlet.dart';
+
+class AtletService {
+  final CollectionReference _atletCollection =
+      FirebaseFirestore.instance.collection('atlet');
+
+  Future<void> addAtlet(Atlet atlet) {
+    return _atletCollection.add(atlet.toMap());
+  }
+
+  Stream<List<Atlet>> getAtletStream() {
+    return _atletCollection.orderBy('nama').snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        return Atlet.fromFirestore(doc);
+      }).toList();
+    });
+  }
+
+  Future<void> updateAtlet(Atlet atlet) {
+    return _atletCollection.doc(atlet.id).update(atlet.toMap());
+  }
+
+  Future<void> deleteAtlet(String atletId) {
+    return _atletCollection.doc(atletId).delete();
+  }
+}
