c8fc66d8e03d5f0a204452f5f6681d8956ba3123
GAJAHGAJAH <1123150165@global.ac.id>
2026-01-08 07:13:13 +0700

    membuat widget _buildTextField untuk email dan password

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -10,6 +10,42 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Text("test"),
+    );
   }
+}
+
+Widget _buildTextField({
+  required String hint,
+  required IconData icon,
+  bool obscure = false,
+  Widget? suffixIcon,
+}) {
+  return Container(
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(30),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.05),
+          blurRadius: 10,
+          offset: const Offset(0, 4),
+        ),
+      ],
+    ),
+    child: TextField(
+      obscureText: obscure,
+      decoration: InputDecoration(
+        hintText: hint,
+        prefixIcon: Icon(icon),
+        suffixIcon: suffixIcon,
+        border: InputBorder.none,
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 18,
+          horizontal: 20,
+        ),
+      ),
+    ),
+  );
 }
