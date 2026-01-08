eab2b46427a744e9bf4dbc1c1658ce55d69466b4
KbDevs12 <daniaku203@gmail.com>
2026-01-08 15:18:55 +0700

    membuat service pelatih

---
+++ lib/services/pelatih_service.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tugas_mobile/models/pelatih.dart';
+
+class PelatihService {
+  final CollectionReference _ref = FirebaseFirestore.instance.collection(
+    'pelatih',
+  );
+
+  Future<void> addPelatih(Pelatih pelatih) async {
+    await _ref.add(pelatih.toMap());
+  }
+
+  Future<void> updatePelatih(String id, Pelatih pelatih) async {
+    await _ref.doc(id).update(pelatih.toMap());
+  }
+
+  Future<void> deletePelatih(String id) async {
+    await _ref.doc(id).delete();
+  }
+
+  Stream<List<Pelatih>> getPelatih() {
+    return _ref.snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        return Pelatih.fromFirestore(doc);
+      }).toList();
+    });
+  }
+}
