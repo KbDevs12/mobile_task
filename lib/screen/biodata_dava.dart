dedbbc830d4a79ac3e8eabf31f0104c9da215447
annddvaa <anandadava93@gmail.com>
2026-01-12 18:51:47 +0700

    menambahkan Positioned dan CircleAvatar untuk tombol back

--- lib/screen/biodata_dava.dart
+++ lib/screen/biodata_dava.dart
@@ -36,6 +36,7 @@ class BiodataDava extends StatelessWidget {
                       ),
                     ),
                   ),
+                  Positioned(child: CircleAvatar()),
                 ],
               ),
             ],
