a022ecb9f12d6bd91d6f15adcd3135fc87682ec8
annddvaa <anandadava93@gmail.com>
2026-01-12 18:18:04 +0700

    menambahkan warna dasar background

--- lib/screen/biodata_dava.dart
+++ lib/screen/biodata_dava.dart
@@ -5,6 +5,8 @@ class BiodataDava extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xffEEF2F7)
+      );
   }
 }
