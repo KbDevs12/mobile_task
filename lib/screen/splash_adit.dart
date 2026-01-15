e8a56d7fd4eba710d08ba444797148de44969ee8
KbDevs12 <daniaku203@gmail.com>
2026-01-15 15:40:10 +0700

    setting animasi

--- lib/screen/splash_adit.dart
+++ lib/screen/splash_adit.dart
@@ -8,3 +8,43 @@ class SplashAdit extends StatefulWidget {
   @override
   State<SplashAdit> createState() => _SplashAditState();
 }
+
+class _SplashAditState extends State<SplashAdit>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scale;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1200),
+    );
+
+    _scale = CurvedAnimation(parent: _controller, curve: Curves.easeOutBack);
+
+    _controller.forward();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const SplashDimas()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    )
+  }
+}
