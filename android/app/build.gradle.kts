cd18fa720e01fc892755e655f38c36e6883c2919
KbDevs12 <daniaku203@gmail.com>
2026-01-15 23:36:53 +0700

    menambahkan core Library

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        coreLibraryDesugaringEnabled true
     }
 
     kotlinOptions {
