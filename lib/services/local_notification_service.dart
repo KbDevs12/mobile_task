3860b80cdc4da90439e0dab6b78a24d7277a201d
KbDevs12 <daniaku203@gmail.com>
2026-01-15 23:23:51 +0700

    menambahkan inisialisasi

--- lib/services/local_notification_service.dart
+++ lib/services/local_notification_service.dart
@@ -1,4 +1,18 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-class LocalNotificationService {}
+class LocalNotificationService {
+  static final FlutterLocalNotificationsPlugin _notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  static Future<void> initialize() async {
+    const AndroidInitializationSettings androidSettings =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    const InitializationSettings settings = InitializationSettings(
+      android: androidSettings,
+    );
+
+    await _notificationsPlugin.initialize(settings);
+  }
+}
