448898c75035ffeeffc73175c05ca52b4789be46
KbDevs12 <daniaku203@gmail.com>
2026-01-15 15:36:30 +0700

    inisiasi splash screen

---
+++ lib/screen/splash_adit.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import './splash_dimas.dart';
+
+class SplashAdit extends StatefulWidget {
+  const SplashAdit({super.key});
+
+  @override
+  State<SplashAdit> createState() => _SplashAditState();
+}
